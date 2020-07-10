6d200b96614a1881dfd9046631fb7075da409667
Nahia <nhaktr@gmail.com>
2020-07-10 17:38:47 -0400

    image carousel banner included

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,10 @@
+/*
+Name: Nahia Akter
+Date: July 10. 2020
+File: Mid term test
+Description: JavaScript file for the html file */
+
+
 // setup your IIFE (Immediately Invoked Function Expression)
 (function () {
 "use strict";
